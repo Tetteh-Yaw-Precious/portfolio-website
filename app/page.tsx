5731a7485fa1cc26ab13287a278b3b0a8766b0e0
Tetteh Yaw Precious <52752056+Tetteh-Yaw-Precious@users.noreply.github.com>
2023-06-18 18:57:39 +0000

    Added loading Page to website

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/homepage/herosection";
+import PageLoader from "@/components/homepage/pageLoader";
 import ProjectSection from "@/components/homepage/projectsection";
 import TestHero from "@/components/homepage/testhero";
 
 export default function Home() {
   return (
     <main className="flex flex-col font-bold">
+      <PageLoader />
       <Herosection />
       {/* <TestHero /> */}
       <ProjectSection />
