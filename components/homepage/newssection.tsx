bbd81b8e76ddda5fe1d943d32618d980e7ece891
Tetteh Yaw Precious <52752056+Tetteh-Yaw-Precious@users.noreply.github.com>
2023-06-18 22:25:23 +0000

    code cleanup

--- components/homepage/newssection.tsx
+++ components/homepage/newssection.tsx
@@ -4,7 +4,7 @@ import NewsCard from './newscard'
 
 const NewsSection = () => {
   return (
-    <div className="flex flex-col items-center gap-12 px-8 py-12 border">
+    <div className="flex flex-col items-center gap-12 px-8 py-12">
       <div>
         <h2 className="text-3xl md:text-6xl heading-text font-sora"><span className="black-radial-gradient">My</span> NewsLetter</h2>
         <p className='md:text-xl text-lg text-center !font-extralight font-outfit'>I love to share my ideas on a few topics ranging from design & more</p>
