0eb228012854e9effb1f020c196e6b08d9d7896f
Tetteh Yaw Precious <52752056+Tetteh-Yaw-Precious@users.noreply.github.com>
2023-06-25 04:39:57 +0000

    setting up project detail page

--- app/projects/[...slug]/page.tsx
+++ app/projects/[...slug]/page.tsx
@@ -1,5 +1,5 @@
 
-'use client'
+// 'use client'
 import { useEffect } from 'react';
 import { groq } from 'next-sanity';
 import { getProject } from '@/sanity/sanity-utils';
@@ -13,12 +13,15 @@ export default async function Page({ params }: props) {
   const slug = params.slug
   console.log(slug[0])
 
-  useEffect(() => {
-    const project = getProject(slug[0])
-    console.log(project)
-  }, [])
-  // const project = getProject(slug)
-  // console.log(project)
+  console.log(slug[0])
+  const project = getProject(slug[0])
+
+  // useEffect(() => {
+  //   console.log(slug[0])
+  //   const project = getProject(slug[0])
+  //   console.log(project)
+  // }, [])
+
   return (
     <div>
 
