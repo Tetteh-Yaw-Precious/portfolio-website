80962f8a96185c5f3c83dc70e5d9ef367415fa59
Tetteh Yaw Precious <52752056+Tetteh-Yaw-Precious@users.noreply.github.com>
2023-06-26 02:21:37 +0000

    animated buttons

--- components/projects/projectSection.tsx
+++ components/projects/projectSection.tsx
@@ -9,13 +9,13 @@ const projectSection = async () => {
     <div className='flex flex-col items-center w-full'>
       <Tabs variant='unstyled' className='flex flex-col items-center w-full gap-8'>
         <TabList className='flex gap-4'>
-          <Tab _selected={{ color: 'red', bg: 'blue.500' }} className='px-12 border-2 md:text-lg text-base text-gray-700 rounded-[0.5rem] font-outfit border-black md:h-14 h-12 shadow-neubrutalism-button font-light aria-selected:!bg-brandblack aria-selected:border-white aria-selected:text-white'>
+          <Tab _selected={{ color: 'red', bg: 'blue.500' }} className='px-12 border-2 button-hover md:text-lg text-base text-gray-700 rounded-[0.5rem] font-outfit border-black md:h-14 h-12 shadow-neubrutalism-button font-light aria-selected:!bg-brandblack aria-selected:border-white aria-selected:text-white'>
             All
           </Tab>
-          <Tab _selected={{ color: 'red', bg: 'brand.wblue.100' }} className='px-12 border-2 md:text-lg text-base text-gray-700 rounded-[0.5rem] font-outfit border-black md:h-14 h-12 shadow-neubrutalism-button font-light aria-selected:!bg-brandblack aria-selected:border-white aria-selected:text-white'>
+          <Tab _selected={{ color: 'red', bg: 'brand.wblue.100' }} className='px-12 border-2 button-hover md:text-lg text-base text-gray-700 rounded-[0.5rem] font-outfit border-black md:h-14 h-12 shadow-neubrutalism-button font-light aria-selected:!bg-brandblack aria-selected:border-white aria-selected:text-white'>
             Design
           </Tab>
-          <Tab _selected={{ color: 'red', bg: 'blue.500' }} className='px-12 border-2 md:text-lg text-base text-gray-700 rounded-[0.5rem] font-outfit border-black md:h-14 h-12 shadow-neubrutalism-button font-light aria-selected:!bg-brandblack aria-selected:border-white aria-selected:text-white'>
+          <Tab _selected={{ color: 'red', bg: 'blue.500' }} className='px-12 border-2 button-hover md:text-lg text-base text-gray-700 rounded-[0.5rem] font-outfit border-black md:h-14 h-12 shadow-neubrutalism-button font-light aria-selected:!bg-brandblack aria-selected:border-white aria-selected:text-white'>
             Frontend Development
           </Tab>
         </TabList>
