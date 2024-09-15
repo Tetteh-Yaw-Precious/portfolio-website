73f373aa1e4b7a0568cda29658a255af22dea9f7
Tetteh Yaw Precious <yawprecious072@gmail.com>
2024-09-15 03:12:07 +0000

    code cleaup

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,20 @@
 // 'use client'
-import Curve from "@/components/Layout/curve";
-import Inner from "@/components/Layout/inner";
 import Herosection from "@/components/homepage/herosection";
 import AboutMe from "@/components/homepage/aboutme";
-import NewsSection from "@/components/homepage/newssection";
 import ProjectSection from "@/components/homepage/projectsection";
-import ResourcesSection from "@/components/homepage/resourcessection";
+
+import Lenis from "lenis";
+// import { useEffect, useState } from "react";
 import { getProjects } from "@/sanity/sanity-utils";
 import { ProjectType } from "@/types/ProjectType";
 
-export async function getServerSideProps() {
-  // Fetch fresh data on each request
-  const projects: ProjectType[] = await getProjects();
-  console.log(projects); // Log to verify data fetching
+export default async function Home() {
+  const projects = await getProjects()
+  console.log(projects)
 
-  return {
-    props: {
-      projects,
-    },
-  };
-}
 
-// Main component
-export default function Home({ projects }: { projects: ProjectType[] }) {
   return (
+
     <div className="flex flex-col font-bold scroll-smooth">
       {/* <PageLoader /> */}
       <div className="scroll-smooth">
@@ -31,8 +22,8 @@ export default function Home({ projects }: { projects: ProjectType[] }) {
         <AboutMe />
         <ProjectSection projects={projects} />
         {/* <NewsSection />
-        <ResourcesSection /> */}
+      <ResourcesSection /> */}
       </div>
     </div>
-  );
+  )
 }
