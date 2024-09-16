2da3e497afeed35191238f17f4d39b83ad59fe3c
Tetteh Yaw Precious <yawprecious072@gmail.com>
2024-09-16 03:47:05 +0000

    updated packages and project details page

--- app/projects/[...slug]/page.tsx
+++ app/projects/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getProject } from '@/sanity/sanity-utils';
 import ProjectDetailPage from '@/components/projectdetailpage';
+import urlFor from '@/library/urlFor';
 type props = {
 	params: { slug: string };
 };
@@ -7,6 +8,16 @@ type props = {
 export default async function Page({ params }: props) {
 	const slug = params.slug;
 	const project = await getProject(slug[0]);
+	// console.log(project.content);
+
+	const imageExample = {
+		asset: {
+			_ref: 'image-8ba07f5ec1269a0d03bdfd31f018c82c407544e9-2000x1000-png',
+			_type: 'reference',
+		},
+	};
+	const imageUrl = urlFor(imageExample).url();
+	console.log(imageUrl);
 
 	return (
 		<>
--- components/projects/richtextcomponents.tsx
+++ components/projects/richtextcomponents.tsx
@@ -1,77 +1,82 @@
-import React, { Children } from 'react'
-import Image from 'next/image'
-import urlFor from '@/library/urlFor'
+import React, { Children } from 'react';
+import Image from 'next/image';
+import urlFor from '@/library/urlFor';
 
 export const myPortableTextComponents = {
-  types: {
-    image: ({ value }: any) => (
-      <div className='md:w-[800px] min-h-[300px]  w-full relative'>
+	types: {
+		image: ({ value }: any) => (
+			<div className='relative md:max-w-screen-lg mx-auto md:my-10 my-5 last:mb-0'>
+				<Image
+					src={urlFor(value)?.url() as string}
+					alt='image'
+					className='object-contain w-full h-auto'
+					layout='responsive'
+					width={1280} // Adjust as needed
+					height={720} // Adjust as needed
+					loading='lazy'
+				/>
+			</div>
+		),
+	},
+	list: {
+		bullet: ({ children }: any) => (
+			<ul className='ml-10 py-5 list-disc space-y-5 tracking-[0px] md:text-base text-sm font-outfit leading-[175%] md:px-[20%] px-[10%]'>
+				{children}
+			</ul>
+		),
+		number: ({ children }: any) => (
+			<ol className='ml-10 py-5 list-decimal space-y-5 tracking-[0px] md:text-base text-sm font-outfit leading-[175%] md:px-[20%] px-[10%]'>
+				{children}
+			</ol>
+		),
+	},
+	block: {
+		h1: ({ children }: any) => (
+			<h1 className='!pt-10 pb-2 leading-[120%] tracking-[-.5px] md:text-xl text-xl font-semibold font-outfit md:px-[20%] px-[10%]'>
+				{children}
+			</h1>
+		),
+		h2: ({ children }: any) => (
+			<h2 className='!pt-10 pb-4 leading-[120%] tracking-[-.5px] md:text-2xl text-lg font-semibold font-outfit md:px-[20%] px-[10%]'>
+				{children}
+			</h2>
+		),
+		h3: ({ children }: any) => (
+			<h3 className='!py-2 leading-[120%] tracking-[-.5px] md:text-xl text-lg font-outfit font-semibold md:px-[20%] px-[10%]'>
+				{children}
+			</h3>
+		),
+		h4: ({ children }: any) => (
+			<h3 className='!py-2 leading-[120%] tracking-[-.5px] md:text-lg text-md font-outfit font-semibold md:px-[20%] px-[10%]'>
+				{children}
+			</h3>
+		),
+		blockquote: ({ children }: any) => (
+			<blockquote className='!py-2 leading-[120%] tracking-[0px] md:text-base text-base border-l-4 border-black font-outfit md:px-[20%] px-[10%]'>
+				{children}
+			</blockquote>
+		),
+		normal: ({ children }: any) => (
+			<p className='!py-2 leading-[175%] tracking-[0px] md:text-base text-sm font-outfit md:px-[20%] px-[10%]'>
+				{children}
+			</p>
+		),
+	},
 
-        <Image
-          src={urlFor(value)?.url() as string}
-          className="object-contain py-10 md:px-[10%] px-[5%]"
-          // width={1280}
-          // height={600}
-          fill
-          loading='lazy'
-          alt="image"
-        />
-      </div>
-    ),
-  },
-  list: {
-    bullet: ({ children }: any) => (
-      <ul className="ml-10 py-5 list-disc space-y-5 tracking-[0px] md:text-base text-sm font-outfit leading-[175%] md:px-[20%] px-[10%]">
-        {children}
-      </ul>
-    ),
-    number: ({ children }: any) => (
-      <ol className="ml-10 py-5 list-decimal space-y-5 tracking-[0px] md:text-base text-sm font-outfit leading-[175%] md:px-[20%] px-[10%]">
-        {children}
-      </ol>
-    ),
-  },
-  block: {
-    h1: ({ children }: any) => (
-      <h1 className="!pt-10 pb-2 leading-[120%] tracking-[-.5px] md:text-xl text-xl font-semibold font-outfit md:px-[20%] px-[10%]">
-        {children}
-      </h1>
-    ),
-    h2: ({ children }: any) => (
-      <h2 className="!pt-10 pb-4 leading-[120%] tracking-[-.5px] md:text-2xl text-lg font-semibold font-outfit md:px-[20%] px-[10%]">
-        {children}
-      </h2>
-    ),
-    h3: ({ children }: any) => (
-      <h3 className="!py-2 leading-[120%] tracking-[-.5px] md:text-xl text-lg font-outfit font-semibold md:px-[20%] px-[10%]">
-        {children}
-      </h3>
-    ),
-    h4: ({ children }: any) => (
-      <h3 className="!py-2 leading-[120%] tracking-[-.5px] md:text-lg text-md font-outfit font-semibold md:px-[20%] px-[10%]">
-        {children}
-      </h3>
-    ),
-    blockquote: ({ children }: any) => (
-      <blockquote className="!py-2 leading-[120%] tracking-[0px] md:text-base text-base border-l-4 border-black font-outfit md:px-[20%] px-[10%]">
-        {children}
-      </blockquote>
-    ),
-    normal: ({ children }: any) => (
-      <p className="!py-2 leading-[177k%] tracking-[0px] md:text-base text-sm font-outfit md:px-[20%] px-[10%]">
-        {children}
-      </p>
-    ),
-  },
-
-  marks: {
-    link: ({ children, value }: any) => {
-      const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined
-      return (
-        <a href={value.href} rel={rel} className="underline text-brandblack hover:font-medium hover:text-blue-800 md:px-[20%] px-[10%]">
-          {children}
-        </a>
-      )
-    },
-  },
-}
+	marks: {
+		link: ({ children, value }: any) => {
+			const rel = !value.href.startsWith('/')
+				? 'noreferrer noopener'
+				: undefined;
+			return (
+				<a
+					href={value.href}
+					rel={rel}
+					className='underline text-brandblack hover:font-medium hover:text-blue-800 md:px-[20%] px-[10%]'
+				>
+					{children}
+				</a>
+			);
+		},
+	},
+};
