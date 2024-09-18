5ee929e7487fcb8b40424f2649b1f761c232db7d
Tetteh Yaw Precious <yawprecious072@gmail.com>
2024-09-18 14:18:36 +0000

    fixed about me section

--- components/homepage/aboutme.tsx
+++ components/homepage/aboutme.tsx
@@ -1,44 +1,62 @@
-import React from 'react'
-import Marquee from 'react-fast-marquee'
-import Image from 'next/image'
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import Image from 'next/image';
 
 const aboutme = () => {
-    const impactedClients = [
-        'impact-clients-01.png', 'impact-clients-02.png', 'impact-clients-03.png', 'impact-clients-04.png', 'impact-clients-05.png', 'impact-clients-06.png', 'impact-clients-07.png'
-    ]
-    return (
-        <div className='md:min-h-[40vh] min-h-[100vh] flex flex-col justify-center md:py-0 scroll-smooth' id='about' >
-            <div className='flex items-center justify-center md:px-[20%] px-[10%] md:py-[8rem] py-[4rem]'>
-                <div className='flex md:flex-row flex-col-reverse md:gap-32 gap-6'>
-                    <div className='!leading-[120%] h-full flex justify-center items-center w-full'>
-                        <p className='md:!text-xl text-base !leading-[190%] md:tracking-[-0.2px] tracking-[-0.2] md:text-left text-center font-outfit text-gray-900 !font-normal'>{
-                            `
-Hello! I’m Yaw Precious, a Product Designer crafting crafting intuitive apps and websites that enhance both user experiences and business operations. My work involves conducting user interviews to uncover key insights, translating them into sweet user experiences, and collaborating closely with developers with my coding expertise when needed.
-I specialize in user research, UI/UX design, and business strategy, delivering data-driven solutions that align with business goals. Recently, I've been diving deeper into frontend development, I'm fascinated by NextJS and AstroJS.
+	const impactedClients = [
+		'impact-clients-01.png',
+		'impact-clients-02.png',
+		'impact-clients-03.png',
+		'impact-clients-04.png',
+		'impact-clients-05.png',
+		'impact-clients-06.png',
+		'impact-clients-07.png',
+	];
+	return (
+		<div
+			className='md:min-h-[40vh] min-h-[100vh] flex flex-col justify-center md:py-0 scroll-smooth'
+			id='about'
+		>
+			<div className='flex items-center justify-center md:px-[20%] px-[10%] md:py-[8rem] py-[4rem]'>
+				<div className='flex md:flex-row flex-col-reverse md:gap-32 gap-6'>
+					<div className='!leading-[120%] h-full flex justify-center items-center w-full'>
+						<p className='md:!text-xl text-base !leading-[190%] md:tracking-[-0.2px] tracking-[-0.2] md:text-left text-center font-outfit text-gray-900 !font-normal'>{`
+                            Hello! I’m Yaw Precious, a Product Designer crafting intuitive apps and websites that enhance user experiences and streamline business operations. I conduct user research, translate insights into impactful designs, and think in systems—focusing on efficiency in both the teams I collaborate with and the artifacts I produce. In my free time, I enjoy tinkering with frontend technologies, currently exploring AstroJS.
         `}</p>
-                    </div>
-                </div>
-            </div>
-            <section className="impact flex md:flex-row flex-col md:px-[10%] px-none px-none md:gap-15 gap-4" style={{
-                background: 'linear-gradient(90deg, rgba(255, 255, 255, 0.05) 0%, rgba(25, 24, 25, 0.05) 67.69%, rgba(255, 255, 255, 0.05) 88.32%)'
-            }}>
-                <div className='md:w-[15%] w-full md:pr-[0rem] pr-0 flex md:items-center md:justify-normal justify-center md:bg-none !md:mb-0 md:mt-0  mt-5'>
-                    <h4 className='font-outfit md:text-lg w-full text-base md:text-start text-center font-semibold leading-[120%]'>Where I’ve made an impact</h4>
-                </div>
-                <div className='Capitalize md:h-[140px] h-[90px] md:w-[80%] w-full flex items-center' >
-                    <Marquee speed={100} pauseOnHover={true}>
-                        {impactedClients.map((item, index) => {
-                            return (
-                                <div key={index} className='px-6'>
-                                    <Image src={`/images/${item}`} alt={'client'} width={135} height={90} />
-                                </div>
-                            )
-                        })}
-                    </Marquee>
-                </div>
-            </section>
-        </div>
-    )
-}
+					</div>
+				</div>
+			</div>
+			<section
+				className='impact flex md:flex-row flex-col md:px-[10%] px-none px-none md:gap-15 gap-4'
+				style={{
+					background:
+						'linear-gradient(90deg, rgba(255, 255, 255, 0.05) 0%, rgba(25, 24, 25, 0.05) 67.69%, rgba(255, 255, 255, 0.05) 88.32%)',
+				}}
+			>
+				<div className='md:w-[15%] w-full md:pr-[0rem] pr-0 flex md:items-center md:justify-normal justify-center md:bg-none !md:mb-0 md:mt-0  mt-5'>
+					<h4 className='font-outfit md:text-lg w-full text-base md:text-start text-center font-semibold leading-[120%]'>
+						Where I’ve made an impact
+					</h4>
+				</div>
+				<div className='Capitalize md:h-[140px] h-[90px] md:w-[80%] w-full flex items-center'>
+					<Marquee speed={100} pauseOnHover={true}>
+						{impactedClients.map((item, index) => {
+							return (
+								<div key={index} className='px-6'>
+									<Image
+										src={`/images/${item}`}
+										alt={'client'}
+										width={135}
+										height={90}
+									/>
+								</div>
+							);
+						})}
+					</Marquee>
+				</div>
+			</section>
+		</div>
+	);
+};
 
-export default aboutme
+export default aboutme;
--- components/homepage/herosection.tsx
+++ components/homepage/herosection.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React, { useRef } from 'react';
 import { animate, delay, motion, useScroll, useTransform } from 'framer-motion';
 import { Button } from '@chakra-ui/react';
@@ -40,34 +40,32 @@ import Image from 'next/image';
 //   },
 // };
 
-
 const container = {
-  initial: { opacity: 0, y: 30 },
-  animate: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: .5,
-      staggerChildren: .5,
-    }
-  }
-}
+	initial: { opacity: 0, y: 30 },
+	animate: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			delay: 0.5,
+			staggerChildren: 0.5,
+		},
+	},
+};
 
 const item = {
-  initial: { opacity: 0, y: 30 },
-  animate: { opacity: 1, y: 0, transition: { delay: 1.3, duration: .2 } }
-}
+	initial: { opacity: 0, y: 30 },
+	animate: { opacity: 1, y: 0, transition: { delay: 1.3, duration: 0.2 } },
+};
 
 const gridContainerVariants = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 1,
-    },
-  },
-}
-
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			staggerChildren: 1,
+		},
+	},
+};
 
 // const svgVariants = {
 //   initial: {
@@ -84,123 +82,159 @@ const gridContainerVariants = {
 //   },
 // };
 
-
 const impactedClients = [
-  'impact-clients-01.png', 'impact-clients-02.png', 'impact-clients-03.png', 'impact-clients-04.png', 'impact-clients-05.png', 'impact-clients-06.png', 'impact-clients-07.png'
-]
+	'impact-clients-01.png',
+	'impact-clients-02.png',
+	'impact-clients-03.png',
+	'impact-clients-04.png',
+	'impact-clients-05.png',
+	'impact-clients-06.png',
+	'impact-clients-07.png',
+];
 
 function Herosection() {
-  const targetRef = useRef<HTMLDivElement>(null)
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-    offset: ["end end", "end start"],
-  })
+	const targetRef = useRef<HTMLDivElement>(null);
+	const { scrollYProgress } = useScroll({
+		target: targetRef,
+		offset: ['end end', 'end start'],
+	});
 
-  const opacity: any = useTransform(scrollYProgress, [0, 10], [1, 0])
-  const scale: any = useTransform(scrollYProgress, [0, 0.5], [1, 0.8])
+	const opacity: any = useTransform(scrollYProgress, [0, 10], [1, 0]);
+	const scale: any = useTransform(scrollYProgress, [0, 0.5], [1, 0.8]);
 
+	return (
+		<main>
+			<motion.div
+				style={{
+					backgroundImage: 'url(/images/background.png)', // Replace with your image path
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+					opacity,
+				}}
+				ref={targetRef}
+				id='home'
+			>
+				<motion.section className=' xl:min-h-[100vh] md:min-h-[100vh] min-h-[100vh] flex flex-col items-center justify-center bg-none md:pb-[10rem] pb-[5rem]'>
+					<motion.div
+						variants={gridContainerVariants}
+						initial='hidden'
+						animate='show'
+						className='flex flex-col items-center justify-center md:gap-6 gap-8 content-container'
+					>
+						<motion.div
+							variants={item}
+							initial='initial'
+							animate='animate'
+							className='px-[1rem] py-[.4rem] rounded-full border-2 border-black flex items-center gap-[.5rem]'
+							style={{
+								borderRadius: '24px',
+								border: '1px solid var(--linear-green, #000000)',
+							}}
+						>
+							<motion.svg
+								initial={{ scale: 1 }}
+								animate={{ scale: [1, 1.2, 1] }}
+								transition={{
+									duration: 1.5,
+									repeat: Infinity,
+									ease: 'easeInOut',
+								}}
+								width='15'
+								height='15'
+								viewBox='0 0 15 15'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<circle
+									cx='7.5'
+									cy='7.5'
+									r='6.5'
+									fill='url(#paint0_linear_1544_9172)'
+									stroke='#CAF5A5'
+								/>
+								<defs>
+									<linearGradient
+										id='paint0_linear_1544_9172'
+										x1='0.5'
+										y1='7.5'
+										x2='14.5'
+										y2='7.5'
+										gradientUnits='userSpaceOnUse'
+									>
+										<stop
+											offset='0.27'
+											stopColor='#66DA00'
+										/>
+										<stop offset='1' stopColor='#E1FBCB' />
+									</linearGradient>
+								</defs>
+							</motion.svg>
 
-  return (
-    <main>
-      <motion.div style={{
-        backgroundImage: 'url(/images/background.png)', // Replace with your image path
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        opacity
-      }}
-        ref={targetRef}
-        id='home'
-      >
-        <motion.section className='xl:min-h-[40.375rem] md:min-h-[20.375rem] min-h-[100vh] flex flex-col items-center justify-center bg-none pb-[5rem]'>
-          <motion.div
-            variants={gridContainerVariants}
-            initial='hidden'
-            animate='show'
-            className="flex flex-col items-center justify-center md:gap-6 gap-8 content-container"
-          >
-            <motion.div
-              variants={item}
-              initial='initial'
-              animate='animate'
-              className='px-[1rem] py-[.4rem] rounded-full border-2 border-black flex items-center gap-[.5rem]'
-              style={{
-                borderRadius: '24px',
-                border: '1px solid var(--linear-green, #000000)',
-              }}
-            >
-              <motion.svg
-                initial={{ scale: 1 }}
-                animate={{ scale: [1, 1.2, 1] }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  ease: "easeInOut",
-                }}
-                width="15"
-                height="15"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="7.5" cy="7.5" r="6.5" fill="url(#paint0_linear_1544_9172)" stroke="#CAF5A5" />
-                <defs>
-                  <linearGradient id="paint0_linear_1544_9172" x1="0.5" y1="7.5" x2="14.5" y2="7.5" gradientUnits="userSpaceOnUse">
-                    <stop offset="0.27" stopColor="#66DA00" />
-                    <stop offset="1" stopColor="#E1FBCB" />
-                  </linearGradient>
-                </defs>
-              </motion.svg>
+							<motion.p
+								variants={item}
+								initial={{ opacity: 0, scale: 0.95 }}
+								animate={{ opacity: 1, scale: 1 }}
+								transition={{
+									duration: 0.6,
+									ease: 'easeOut',
+								}}
+								className='md:text-base text-base italic !text-[#011019] md:leading-[110%] leading-[130%] text-center font-normal font-outfit md:!tracking-[-.1px] tracking-[-.2px]'
+								style={{
+									background:
+										'var(--gradient-stroke, linear-gradient(93deg, #000 4.71%, #727272 120.19%))',
+									backgroundClip: 'text',
+									WebkitBackgroundClip: 'text',
+									WebkitTextFillColor: 'transparent',
+								}}
+							>
+								{`Open to new opportunities`}
+							</motion.p>
+						</motion.div>
 
-              <motion.p
-                variants={item}
-                initial={{ opacity: 0, scale: 0.95 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                  duration: 0.6,
-                  ease: "easeOut",
-                }}
-                className='md:text-base text-base italic !text-[#011019] md:leading-[110%] leading-[130%] text-center font-normal font-outfit md:!tracking-[-.1px] tracking-[-.2px]'
-                style={{
-                  background: 'var(--gradient-stroke, linear-gradient(93deg, #000 4.71%, #727272 120.19%))',
-                  backgroundClip: 'text',
-                  WebkitBackgroundClip: 'text',
-                  WebkitTextFillColor: 'transparent',
-                }}
-              >
-                {`Open to new opportunities`}
-              </motion.p>
-            </motion.div>
-
-            <motion.h1
-              variants={item}
-              initial='initial'
-              animate='animate'
-              className='md:text-5xl text-2xl !text-[#011019] md:leading-[110%] leading-[130%] text-center font-light font-outfit md:!tracking-[-1.5px] tracking-[-.5px] md:w-[60%] w-[70%]'
-              style={{
-                scale: scale,
-              }}
-            >
-              A Product Designer with focus on
-              <span className='font-medium'> UX Research,</span><span className='font-medium'> </span> <span className='font-medium'> business strategy & Engineering </span>
-            </motion.h1>
-            <motion.div variants={item}>
-              {/* <Button className='font-outfit font-medium md:text-2xl text-lg md:px-16 px-10 py-8 md:!py-12 rounded-full' style={{
+						<motion.h1
+							variants={item}
+							initial='initial'
+							animate='animate'
+							className='md:text-5xl text-2xl !text-[#011019] md:leading-[110%] leading-[130%] text-center font-light font-outfit md:!tracking-[-1.5px] tracking-[-.5px] md:w-[70%] w-[70%]'
+							style={{
+								scale: scale,
+							}}
+						>
+							A Product Designer with focus on
+							<span className='font-medium'> UX Research,</span>
+							<span className='font-medium'> </span>{' '}
+							<span className='font-medium'>
+								{' '}
+								business strategy & Engineering{' '}
+							</span>
+						</motion.h1>
+						<motion.div variants={item}>
+							{/* <Button className='font-outfit font-medium md:text-2xl text-lg md:px-16 px-10 py-8 md:!py-12 rounded-full' style={{
                 background: ' linear-gradient(90deg, #66DA00 27%, #E1FBCB 100%)',
               }}>{`Let's talk 👋`}
               </Button> */}
-            </motion.div>
-            {/* <motion.p variants={item} className='xl:text-xl px-[10%] xl:px-0 font-normal text-center !text-base text-gray-900'> Scroll to see selected works</motion.p> */}
-            <motion.svg variants={item} className='w-6 h-6 stroke-[2px] md:w-8 md:h-6 animate-bounce text-gray-950' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
-            </motion.svg>
-
-          </motion.div>
-        </motion.section>
-      </motion.div>
-
-    </main>
-
-  );
+						</motion.div>
+						{/* <motion.p variants={item} className='xl:text-xl px-[10%] xl:px-0 font-normal text-center !text-base text-gray-900'> Scroll to see selected works</motion.p> */}
+						<motion.svg
+							variants={item}
+							className='w-6 h-6 stroke-[2px] md:w-8 md:h-6 animate-bounce text-gray-950'
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke-width='1'
+							stroke='currentColor'
+						>
+							<path
+								stroke-linecap='round'
+								stroke-linejoin='round'
+								d='M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3'
+							/>
+						</motion.svg>
+					</motion.div>
+				</motion.section>
+			</motion.div>
+		</main>
+	);
 }
 
 export default Herosection;
