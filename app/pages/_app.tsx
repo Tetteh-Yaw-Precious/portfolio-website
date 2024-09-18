5b5d12772e4b85adae17aec2b37e4098264977c5
Tetteh Yaw Precious <yawprecious072@gmail.com>
2024-09-18 23:37:39 +0000

    Added vercel analytics

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,21 +1,22 @@
-import dynamic from 'next/dynamic'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '@/library/theme-config'
-import { AnimatePresence } from 'framer-motion'
-import Topnavbar from '@/components/topnavbar'
+import dynamic from 'next/dynamic';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '@/library/theme-config';
+import { AnimatePresence } from 'framer-motion';
+import Topnavbar from '@/components/topnavbar';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  return (
-
-    <ChakraProvider theme={theme} >
-      <AnimatePresence mode='wait'>
-        <Component key={router.route} {...pageProps} />
-      </AnimatePresence>
-    </ChakraProvider>
-  )
+	return (
+		<ChakraProvider theme={theme}>
+			<AnimatePresence mode='wait'>
+				<Component key={router.route} {...pageProps} />
+			</AnimatePresence>
+			<Analytics />
+		</ChakraProvider>
+	);
 }
 
 export default dynamic(() => Promise.resolve(MyApp), {
-  ssr: false
-})
+	ssr: false,
+});
