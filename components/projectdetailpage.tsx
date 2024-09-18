f3951cd707987c25bf62ee8f0ff84dbbddc2ad83
Tetteh Yaw Precious <yawprecious072@gmail.com>
2024-09-18 12:28:50 +0000

    a little change here and there

--- components/projectdetailpage.tsx
+++ components/projectdetailpage.tsx
@@ -9,7 +9,7 @@ import { teamMember } from '@/sanity/schemas/project-schemas';
 
 const ProjectDetailPage = ({ viewProject }: any) => {
 	const project: ProjectType = viewProject;
-	console.log(project.teamMembers);
+	console.log(project);
 
 	const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
 
